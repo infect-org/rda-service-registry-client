7b5aefa3e1c2010dc05fadcfefd62c52d53056b6
Michael van der Weg <michael@joinbox.com>
2019-10-04 14:28:53 +0200

    chore: upgrade dependecies

--- test/100.000-service-registry-client.js
+++ test/100.000-service-registry-client.js
@@ -1,7 +1,7 @@
 import section from 'section-tests';
 import assert from 'assert';
 import ServiceManager from '@infect/rda-service-manager';
-import ServiceRegistryClient from '../src/ServiceRegistryClient.mjs';
+import ServiceRegistryClient from '../src/ServiceRegistryClient.js';
 
 
 
@@ -14,10 +14,10 @@ section('RDA Service Registry Client', (section) => {
 
     section.setup(async() => {
         sm = new ServiceManager({
-            args: '--dev --log-level=error+ --log-module=*'.split(' '),
+            args: '--dev.testing --log-level=error+ --log-module=*'.split(' '),
         });
 
-        await sm.startServices('rda-service-registry');
+        await sm.startServices('@infect/rda-service-registry');
     });
 
 
